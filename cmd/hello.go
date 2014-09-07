74a6db10b06193d37feece5b1057457979b97dcc
Joel Scoble <joel.scoble@outlook.com>
2014-09-07 12:31:27 -0500

    committing the moved files for the cmd rename, forgot on prior commit

---
+++ cmd/hello.go
@@ -0,0 +1,33 @@
+// hello runs the hello command
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func Hello(words ...string) (string, error) {
+	logger.Tracef("Entering Hello with: %v\n", words)
+
+	h := "Hello"
+	if len(words) == 0 {
+		logger.Tracef("exiting Hello: h  = %v, err=nil\n", h)
+		return h, nil
+	}
+
+	for _, word := range words {
+		h += " " + word
+	}
+
+	// TODO: This should really be fed through strconv.FormatBool
+	if os.Getenv("lower") == "true" {
+		h = strings.ToLower(h)
+	}
+
+	fmt.Println(h)
+	logger.Infof("output: %s", h)
+
+	message := fmt.Sprintf("Hello completed.\nLower used: %v.\nPhrase used for Hello: %s.\n",  os.Getenv("lower"), h)   
+	return message, nil
+}
