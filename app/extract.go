bc70329d6a0e106cbb5d97b50a208f1429799d40
Joel Scoble <joel.scoble@outlook.com>
2015-08-31 18:18:12 -0500

    have extract determine whether the file is a zip or not and act accordinly. This means call zip's extract or tar's extract. Files to be extracted is expected to contain an archive of either format.

--- app/extract.go
+++ app/extract.go
@@ -2,15 +2,22 @@ package app
 
 import (
 	"fmt"
+	"path"
+	"strings"
 
 	car "github.com/mohae/carchivum"
 )
 
 func Extract(src string) (string, error) {
 	fmt.Printf("\nExtract %q", src)
-	err := car.Extract(src)
-	if err != nil {
-		return "", err
+	// check the extension: use zip for .zip and tar for everything else
+	ext := strings.ToLower(path.Ext(src))
+	if ext == ".zip" {
+		z := car.NewZip(src)
+		z.OutDir = contour.GetString("output_dir")
+		return z.Extract()
 	}
-	return fmt.Sprintf("%s extracted", src), nil
+	t := car.NewTar(src)
+	t.OutDir = contour.GetString("output_dir")
+	return t.Extract()
 }
